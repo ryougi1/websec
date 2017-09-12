bcb9f801ffba62c7f065e2ea99feaa88466cfdc3
dic15san <dic15san@student.lu.se>
2017-09-12 12:06:56 +0200

    login page

---
+++ login.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>Login page</title>
+
+<link rel="stylesheet" type="text/css" href="css/main2.css">
+
+</head>
+<body>
+	<div class = "Login">
+		<h1> Login </h1>
+		<form method = "post">
+			<input type="text" name="n" placeholder="Username" required="required"/>
+			<input type="password" name="p" placeholder="Password" required="required"/>
+			<button type="submit" class="btn btn-primary btn-block btn-large"> Login </button> 
+		</form>
+	</div>
+
+</body>
+</html>
