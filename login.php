665bec394605da44b74fa05023226e058cea19ba
dic15san <dic15san@student.lu.se>
2017-09-15 15:21:36 +0200

    login & register

--- login.php
+++ login.php
@@ -1,19 +1,123 @@
 <html>
-<head>
-<title>Login page</title>
 
-<link rel="stylesheet" type="text/css" href="css/main2.css">
+<h1> Web shop </h1>
 
-</head>
+<h2>
+<style>
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+li a:hover {
+    background-color: #111;
+}
+</style>
+</h2>
 <body>
-	<div class = "Login">
-		<h1> Login </h1>
-		<form method = "post">
-			<input type="text" name="n" placeholder="Username" required="required"/>
-			<input type="password" name="p" placeholder="Password" required="required"/>
-			<button type="submit" class="btn btn-primary btn-block btn-large"> Login </button> 
-		</form>
-	</div>
+
+<ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li style="float:right"><a href="#about">To checkout</a></li>
+  <li style="float:right"><a href="#about">Log in</a></li>
+  <li style="float:right"><a href="#about">Sign up</a></li>
+</ul>
+
+</body>
+
+<style>
+form {
+    border: 3px solid #f1f1f1;
+}
+
+input[type=text], input[type=password] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+}
+
+button {
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+}
+
+button:hover {
+    opacity: 0.8;
+}
+
+.cancelbtn {
+    width: auto;
+    padding: 10px 18px;
+    background-color: #f44336;
+}
+
+.container {
+    padding: 16px;
+}
+
+span.psw {
+    float: right;
+    padding-top: 16px;
+}
+
+/* Change styles for span and cancel button on extra small screens */
+@media screen and (max-width: 300px) {
+    span.psw {
+       display: block;
+       float: none;
+    }
+    .cancelbtn {
+       width: 100%;
+    }
+}
+</style>
+<body>
+
+<h2>Login Form</h2>
+
+<form action="/action_page.php">
+
+  <div class="container">
+    <label><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
+
+    <label><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+        
+    <button type="submit">Login</button>
+    <input type="checkbox" checked="checked"> Remember me
+  </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">Cancel</button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+</form>
 
 </body>
 </html>
---
+++ register.php
@@ -0,0 +1,152 @@
+<html>
+<h1>Webshop</h1>
+
+<h2>
+<style>
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+li a:hover {
+    background-color: #111;
+}
+</style>
+</h2>
+
+
+<ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li style="float:right"><a href="#about">To checkout</a></li>
+  <li style="float:right"><a href="#about">Log in</a></li>
+  <li style="float:right"><a href="#about">Sign up</a></li>
+</ul>
+
+</body>
+
+
+
+
+
+
+
+
+<style>
+/* Full-width input fields */
+input[type=text], input[type=password] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+}
+
+/* Set a style for all buttons */
+button {
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+}
+
+/* Extra styles for the cancel button */
+.cancelbtn {
+    padding: 14px 20px;
+    background-color: #f44336;
+    float: left;
+    width: 25%;
+}
+
+/* Float cancel and signup buttons and add an equal width */
+.signupbtn {
+    float: right;
+    width: 25%;
+}
+
+/* Add padding to container elements */
+.container {
+    padding: 100px;
+}
+
+/* Clear floats */
+.clearfix::after {
+    content: "";
+    clear: both;
+    display: table;
+}
+
+/* Change styles for cancel button and signup button on extra small screens */
+@media screen and (max-width: 300px) {
+    .cancelbtn, .signupbtn {
+       width: 100%;
+    }
+}
+</style>
+<body>
+
+<h2>Signup Form</h2>
+
+<form action="/action_page.php" style="border:1px solid #ccc">
+  <div class="container">
+    
+     <label><b>First Name</b></label>
+    <input type="text" placeholder="Enter First Name" name="name" required>
+
+     <label><b>Last Name</b></label>
+    <input type="text" placeholder="Enter Last Name" name="name" required>
+
+    <label><b>Email</b></label>
+    <input type="text" placeholder="Enter Email" name="email" required>
+
+    <label><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
+    <label><b>Repeat Password</b></label>
+    <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+
+    <label><b>Address</b></label>
+    <input type="text" placeholder="Enter Address" name="address" required>
+
+    <label><b>Zipcode</b></label>
+    <input type="text" placeholder="Enter Zipcode" name="zipcode" required>
+
+    <label><b>City</b></label>
+    <input type="text" placeholder="Enter City" name="city" required>
+
+    <label><b>Country</b></label>
+    <input type="text" placeholder="Enter Country" name="country" required>
+    
+
+    <input type="checkbox" checked="checked"> Remember me
+    <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
+
+    <div class="clearfix">
+      <button type="button" class="cancelbtn">Cancel</button>
+      <button type="submit" class="signupbtn">Sign Up</button>
+    </div>
+  </div>
+</form>
+
+</body>
+</html>
