d54171f9042712a58b36b2f228f3ae698b543168
James Gu <17310662+ryougi1@users.noreply.github.com>
2017-09-19 11:24:41 +0200

    Update init.sql
    Fixed so datatbase init actually works

--- init.sql
+++ init.sql
@@ -1,21 +1,23 @@
 -- paste this into command form to init DB
 CREATE DATABASE Webshop;
 CREATE TABLE Users (
-	   uuid	 	 int PRIMARY AUTO_INCREMENT,
-	   email     tinytext UNIQUE,
+	   uuid	 	 int NOT NULL AUTO_INCREMENT,
+	   email     varchar(100) UNIQUE,
 	   firstname tinytext NOT NULL,
 	   lastname  tinytext NOT NULL,
 	   country	 tinytext NOT NULL,
 	   city		 tinytext NOT NULL,
 	   zip		 int NOT NULL,
 	   address	 tinytext NOT NULL,
-	   pwhash	 tinytext NOT NULL
+	   pwhash	 tinytext NOT NULL,
+	PRIMARY KEY (uuid)
 );
 
 CREATE TABLE Products (
-	   prodID    int PRIMARY AUTO_INCREMENT,
+	   prodID    int NOT NULL AUTO_INCREMENT,
 	   prodName	 tinytext NOT NULL,
 	   prodCost	 int NOT NULL,
 	   prodStock int NOT NULL,
-	   prodImage tinytext
+	   prodImage tinytext, 
+	PRIMARY KEY (prodID)
 );
