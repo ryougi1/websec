a308319ef01e575685bf235d91963909d6ddbd8f
Mecha_Magpie <dat13egr@student.lu.se>
2017-09-19 10:50:46 +0200

    made logins work

--- loginhandler.php
+++ loginhandler.php
@@ -4,10 +4,11 @@
   <?php
 	 $db = new mysqli("localhost", "root", "", "Webshop");
 	 $stmt = $db->prepare("SELECT pwhash FROM Users WHERE email = ?;");
-	 $stmt->bind_param("s", $_POST["email"]);
-	 $stmt->execute();
-	 $pwhash = $stmt->fetch;
-	 if (password_verify($pwhash, $_POST["psw"])) {
+     $stmt->bind_param("s", $_POST["email"]);
+     $stmt->execute();
+     $stmt->bind_result($pwhash);
+     $stmt->fetch();
+	 if (password_verify($_POST["psw"], $pwhash)) {
 	 echo "valid password!";
 	 } else {
 	 echo "wrong password!";
