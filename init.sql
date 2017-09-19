e1ba44af536d353f1a476d5f11feb610739cde8d
James Gu <17310662+ryougi1@users.noreply.github.com>
2017-09-19 11:11:56 +0200

    Update init.sql
    Added primary keys and not null to database

--- init.sql
+++ init.sql
@@ -1,21 +1,21 @@
 -- paste this into command form to init DB
 CREATE DATABASE Webshop;
 CREATE TABLE Users (
-	   uuid	 	 int,
-	   email     tinytext,
-	   firstname tinytext,
-	   lastname  tinytext,
-	   country	 tinytext,
-	   city		 tinytext,
-	   zip		 int,
-	   address	 tinytext,
-	   pwhash	 tinytext
+	   uuid	 	 int PRIMARY AUTO_INCREMENT,
+	   email     tinytext UNIQUE,
+	   firstname tinytext NOT NULL,
+	   lastname  tinytext NOT NULL,
+	   country	 tinytext NOT NULL,
+	   city		 tinytext NOT NULL,
+	   zip		 int NOT NULL,
+	   address	 tinytext NOT NULL,
+	   pwhash	 tinytext NOT NULL
 );
 
 CREATE TABLE Products (
-	   prodID    int,
-	   prodName	 tinytext,
-	   prodCost	 int,
-	   prodStock int,
+	   prodID    int PRIMARY AUTO_INCREMENT,
+	   prodName	 tinytext NOT NULL,
+	   prodCost	 int NOT NULL,
+	   prodStock int NOT NULL,
 	   prodImage tinytext
 );
