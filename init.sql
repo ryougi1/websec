3ec278635caeda89c14940482127a23f9c801744
James Gu <17310662+ryougi1@users.noreply.github.com>
2017-09-27 12:09:53 +0200

    Added password blacklist

--- init.sql
+++ init.sql
@@ -21,3 +21,21 @@ CREATE TABLE Products (
   prodImage tinytext,
   PRIMARY KEY (prodID)
 );
+
+CREATE TABLE PwBlacklist (
+    primkey int NOT NULL AUTO_INCREMENT,
+  password  varchar(10) NOT NULL,
+  PRIMARY KEY (primkey)
+  );
+  
+  INSERT INTO PwBlacklist (password) VALUES 
+  ("Password12"), 
+ ("passworD12"), 
+ ("pASSWORD12"), 
+ ("PASSWORd12"), 
+ ("Abcdefgh1j"),
+ ("ABCDEFGH1j"),
+ ("4Bcdefghij"), 
+ ("abcd3Fghij"), 
+ ("12345678Aa"), 
+ ("Aa12345678");
