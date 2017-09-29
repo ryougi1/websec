40c0b9881c0e81e9987d4f951e5090595066b5ae
James Gu <17310662+ryougi1@users.noreply.github.com>
2017-09-29 11:35:12 +0200

    signuphandler: also checks with a pw blacklist

--- signuphandler.php
+++ signuphandler.php
@@ -3,69 +3,73 @@
   <?php include 'header.php'; ?>
   <body>
     <?php
-    $db = new mysqli(
+     $db = new mysqli(
       ini_get("mysqli.default_host"),
       ini_get("mysqli.default_user"),
       ini_get("mysqli.default_pw"),
       "Webshop");
     check_token('register');
+    
+/* Error handlers*/
 
+//Make sure passwords match   
+if(!($_POST["psw"] === $_POST["psw-repeat"])) {
+  header('location:register.php?signup=nomatchpw');
+  die();
+}
 
-    /* Error handlers*/
+//Make sure password length >= 10, includes one upper case, one lower case and a digit
 
-    //Make sure password length >= 10, includes one upper case, one lower case and a digit
-    if(!preg_match("/(?=.*[a-Z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{10,}/", $_POST["psw"])) {
-      header('location:register.php?signup=weakpw');
-      die();
-    }
-    //Make sure passwords match
+if(!preg_match("/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{10,}/", $_POST["psw"])) {
+  header('location:register.php?signup=weakpw');
+  die();
+}
 
-    if(!($_POST["psw"] === $_POST["psw-repeat"])) {
-      header('location:register.php?signup=nomatchpw');
-      die();
-    }
+//Check password against password blacklist
+$stmt = $db->prepare("SELECT password FROM PwBlacklist WHERE password = ?;");
+$stmt->bind_param("s", $_POST["psw"]);
+$stmt->execute();
+$stmt->bind_result($blacklistedPw);
+$stmt->fetch();
+if ($blacklistedPw === $_POST["psw"]) {
+    $stmt->close();
+    header('location:register.php?signup=passwordtoocommon');
+    die();
+}
 
-    //Make sure password length >= 10, includes one upper case, one lower case and a digit
-
-    if(!preg_match("/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{10,}/", $_POST["psw"])) {
-      header('location:register.php?signup=weakpw');
-      die();
-    }
-
-    //Check valid email, also check if email already in use
-    if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
-      header('location:register.php?signup=invalidemail');
-      die();
-    }else {
-      $stmt = $db->prepare("SELECT email FROM Users WHERE email = ?;");
-      $stmt->bind_param("s", $_POST["email"]);
-      $stmt->execute();
-      $stmt->bind_result($dbEmail);
-      $stmt->fetch();
-      if ($dbEmail === $_POST["email"]) {
-        $stmt->close();
-        header('location:register.php?signup=emailinuse');
-        die();
-      }
-    }
-
-    $stmt3 = $db->prepare("INSERT INTO Users
+//Check valid email, also check if email already in use
+if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+  header('location:register.php?signup=invalidemail');
+  die();
+}else {
+  $stmt1 = $db->prepare("SELECT email FROM Users WHERE email = ?;");
+    $stmt1->bind_param("s", $_POST["email"]);
+    $stmt1->execute();
+    $stmt1->bind_result($dbEmail);
+    $stmt1->fetch();
+  if ($dbEmail === $_POST["email"]) {
+    $stmt1->close();
+    header('location:register.php?signup=emailinuse');
+    die();
+  }
+}
+  
+$stmt2 = $db->prepare("INSERT INTO Users
           (email, firstname, lastname, country, city, zip, address, pwhash)
           VALUES (?, ?, ?, ?, ?, ?, ?, ?);");
-    $stmt3->bind_param("sssssiss",
-                       $_POST["email"],
-                       $_POST["fname"],
-                       $_POST["lname"],
-                       $_POST["country"],
-                       $_POST["city"],
-                       $_POST["zipcode"],
-                       $_POST["address"],
-                       $pwhash);
-    $pwhash = password_hash($_POST["psw"], PASSWORD_DEFAULT);
-    $stmt3->execute();
-    $stmt3->close();
-
-    echo "Sign up successfull";
+$stmt2->bind_param("sssssiss",
+                      $_POST["email"],
+                      $_POST["fname"],
+                      $_POST["lname"],
+                      $_POST["country"],
+                      $_POST["city"],
+                      $_POST["zipcode"],
+                      $_POST["address"],
+                      $pwhash);
+$pwhash = password_hash($_POST["psw"], PASSWORD_DEFAULT);
+$stmt2->execute();
+$stmt2->close();
+echo "Sign up successfull";
     ?>
   </body>
 </html>
