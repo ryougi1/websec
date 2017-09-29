005b1f5916ca3961fc1583fbd7a162fe58ef5863
James Gu <17310662+ryougi1@users.noreply.github.com>
2017-09-29 11:02:37 +0200

    signuphandler: checks email, checks pw match, checks pw strength

--- signuphandler.php
+++ signuphandler.php
@@ -3,43 +3,43 @@
   <?php include 'header.php'; ?>
   <body>
     <?php
-
      $db = new mysqli(
       ini_get("mysqli.default_host"),
       ini_get("mysqli.default_user"),
       ini_get("mysqli.default_pw"),
       "Webshop");
-
     if (!($_POST["form-token"] === $_SESSION["form-token"])) {
       header('location:register.php?msg=expired');
       die();
     }
-
     
 /* Error handlers*/
 
-//Make sure password length >= 10, includes one upper case, one lower case and a digit
-if(!preg_match("/(?=.*[a-Z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{10,}/", $_POST["psw"])) {
-  header('location:register.php?signup=weakpw');
-  die();
-}
 //Make sure passwords match   
 if(!($_POST["psw"] === $_POST["psw-repeat"])) {
   header('location:register.php?signup=nomatchpw');
   die();
 }
+
+//Make sure password length >= 10, includes one upper case, one lower case and a digit
+
+if(!preg_match("/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{10,}/", $_POST["psw"])) {
+  header('location:register.php?signup=weakpw');
+  die();
+}
+
 //Check valid email, also check if email already in use
 if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
   header('location:register.php?signup=invalidemail');
   die();
 }else {
-  $stmt2 = $db->prepare("SELECT * FROM users WHERE email = ?;");
-  $stmt2->bind_param($_POST["email"]);
-  $stmt2->execute();
-  mysqli_stmt_store_result($stmt2);
-  $resultCheck = mysqli_stmt_num_rows($stmt2);
-  if ($resultCheck > 0) {
-    $stmt2->close();
+  $stmt = $db->prepare("SELECT email FROM Users WHERE email = ?;");
+    $stmt->bind_param("s", $_POST["email"]);
+    $stmt->execute();
+    $stmt->bind_result($dbEmail);
+    $stmt->fetch();
+  if ($dbEmail === $_POST["email"]) {
+    $stmt->close();
     header('location:register.php?signup=emailinuse');
     die();
   }
@@ -60,8 +60,8 @@ $stmt3->bind_param("sssssiss",
 $pwhash = password_hash($_POST["psw"], PASSWORD_DEFAULT);
 $stmt3->execute();
 $stmt3->close();
+    
 echo "Sign up successfull";
-
     ?>
   </body>
 </html>
