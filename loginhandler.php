8e71e76a0e2c720caa8d922165cd8c9d651e4c95
Mecha_Magpie <dat13egr@student.lu.se>
2017-10-01 10:06:19 +0200

    cleaned up commented-out code

--- loginhandler.php
+++ loginhandler.php
@@ -57,34 +57,4 @@ else {
     }
   }
 }
-/* if (password_verify($_POST["psw"], $pwhash)) {
-  session_regenerate_id();
-  session_regenerate_form_token();
-  $_SESSION['email'] = $_POST["email"];
-  $_SESSION['auth'] = true;
-  header('location:index.php');
-} else {
-  mysqli_report(MYSQLI_REPORT_ALL);
-  $stmt = $db->prepare("SELECT UNIX_TIMESTAMP(lastfail), fails FROM Users WHERE email = ?;");
-  $stmt->bind_param("s", $_POST['email']);
-  $stmt->execute();
-  $stmt->bind_result($lastfail, $fails);
-  $stmt->fetch();
-  $stmt->close();
-  if ($lastfail > time() - 20 * 2 ** $fails) {
-    $fails += 1;
-  } else {
-    $fails = 1;
-  }
-  $lastfail = time();
-  $stmt = $db->prepare("UPDATE Users SET lastfail = FROM_UNIXTIME(?), fails = ? WHERE email = ?;");
-  $stmt->bind_param("iis", $lastfail, $fails, $_POST['email']);
-  $stmt->execute();
-  {
-    echo "$lastfail + 20 * 2 ** $fails}";
-    die();
-  }
-  $lockout = $lastfail + 20 * 2 ** $fails;
-  header("location:login.php?msg=authfail&unlocks=$lockout");
-}*/
 ?>
