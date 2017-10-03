6cf358fc62c539794e85987b7f4371bd7e74c923
Mecha_Magpie <dat13egr@student.lu.se>
2017-10-03 13:30:10 +0200

    added CSRF token to log out

--- header.php
+++ header.php
@@ -10,7 +10,13 @@
     <li style="float:right;font-size:18px;color:#f2f2f2;"> <a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span></a> </li>
     <?php endif; ?>
     <?php if (isset($_SESSION['auth']) && $_SESSION['auth'] === true): ?>
-    <li style="float:right"><a href="logouthandler.php">Sign out</a></li>
+    <li style="float:right">
+      <form action="logouthandler.php" method="post">
+        <?php insert_token(); ?>
+        <button type="submit">Sign out</button>
+      </form>
+    </li>
+    <!--<li style="float:right"><a href="logouthandler.php">Sign out</a></li>-->
     <li style="float:right">Signed in as <?php echo $_SESSION['email'] ?></li>
     <?php else: ?>
     <li style="float:right"><a href="login.php">Log in</a></li>
