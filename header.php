fa57b088e442f7e7db1d2ebc1fde822f7a87fe04
Mecha_Magpie <dat13egr@student.lu.se>
2017-10-04 18:14:05 +0200

    added some XSS protection

--- header.php
+++ header.php
@@ -2,7 +2,6 @@
 <h2>
   <link rel="stylesheet" href="header.css">
 </h2>
-<body>
   <ul>
     <li><a class="active" href="index.php">Home</a></li>
     <!--<li style="float:right"><a href="#about">To checkout</a></li>-->
@@ -17,10 +16,9 @@
       </form>
     </li>
     <!--<li style="float:right"><a href="logouthandler.php">Sign out</a></li>-->
-    <li style="float:right">Signed in as <?php echo $_SESSION['email'] ?></li>
+    <li style="float:right">Signed in as <?php echo htmlentities($_SESSION['email']) ?></li>
     <?php else: ?>
     <li style="float:right"><a href="login.php">Log in</a></li>
     <li style="float:right"><a href="register.php">Sign up</a></li>
     <?php endif; ?>
   </ul>
-</body>
--- receipt.php
+++ receipt.php
@@ -8,10 +8,28 @@ check_token('cart')
     <?php include 'header.php' ?>
     <h2>Purchase receipt</h2>
     <link rel="stylesheet" href="webshop.css">
-    <?php include 'cartview.php';
-    $_SESSION['lastpurchase'] = $_SESSION['cart'];
-    $_SESSION['cart'] = [];
-    make_cart($_SESSION['lastpurchase']);
+    <?php
+  include 'cartview.php';
+$_SESSION['lastpurchase'] = $_SESSION['cart'];
+$_SESSION['cart'] = [];
+make_cart($_SESSION['lastpurchase']);
+$db = mysqli_connect(
+  ini_get("mysqli.default_host"),
+  ini_get("mysqli.default_user"),
+  ini_get("mysqli.default_pw"),
+  "Webshop");
+$stmt = $db->prepare("SELECT firstname, lastname, address, zip, city, country FROM Users WHERE email = ?;");
+$stmt->bind_param("s", $_SESSION['email']);
+$stmt->execute();
+$stmt->bind_result($firstname, $lastname, $address, $zip, $city, $country);
+$stmt->fetch();
+$stmt->close();;
     ?>
+    <p>Shipping to:<br>
+      <?= htmlentities("$firstname $lastname") ?><br>
+      <?= htmlentities($address) ?><br>
+      <?= htmlentities("$zip $city") ?><br>
+      <?= htmlentities($country) ?>
+    </p>
   </body>
 </html>
